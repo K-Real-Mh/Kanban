fd4e9ac7741ba3044673ed069766cc4ee1c37de8
Kirill Makhov <mmahovkirill@gmail.com>
2022-07-20 02:25:46 +0300

    feat: add new components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import s from './App.module.scss';
+import Header from './components/Header/Header';
+import Sidebar from './components/Sidebar/Sidebar.jsx';
 
 function App() {
   return (
     <div className={s.App}>
-        <header className={s.App__header}></header>
-        <aside className={s.App__sidebar}></aside>
+        <Header className={s.App__header} />
+        <Sidebar className={s.App__sidebar} />
         <main className={s.App__main}></main>
     </div>
   )
