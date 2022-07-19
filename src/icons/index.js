4d02519c70f5ea194e4c6319a660040ec388e7ea
Kirill Makhov <mmahovkirill@gmail.com>
2022-07-20 02:22:47 +0300

    feat: add icons

---
+++ src/icons/index.js
@@ -0,0 +1,8 @@
+export { default as LogoIcon } from './LogoIcon';
+export { default as BellIcon } from './BellIcon';
+export { default as ArrowDownIcon } from './ArrowDownIcon';
+export { default as SearchIcon } from './SearchIcon';
+export { default as PlusIcon } from './PlusIcon';
+export { default as TableIcon } from './TableIcon';
+export { default as ArrowRightIcon } from './ArrowRightIcon';
+export { default as QuestionIcon } from './QuestionIcon';
