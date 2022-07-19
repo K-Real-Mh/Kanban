bf021e7e99659a93bde22a9e4a87d006a6759c45
Kirill Makhov <mmahovkirill@gmail.com>
2022-07-20 02:24:00 +0300

    feat: create Account component

---
+++ src/components/Account/Account.jsx
@@ -0,0 +1,30 @@
+import Button from '../Button/Button';
+import Icon from '../Icon/Icon';
+import Avatar from '../Avatar/Avatar';
+import numberWithSpaces from '../../helpers/numberWithSpaces';
+import s from './Account.module.scss';
+
+function Account({ data }) {
+    return (
+        <div className={s.Account}>
+            <span className={s.Account__price}>
+                <span>{numberWithSpaces(data.sum)}</span>
+                <span>{data.currency}</span>
+            </span>
+            <div className={s.Account__button}>
+                <Button ariaLabel="Notifications" icon={
+                    <Icon name="BellIcon" color="#B8B8B8" width="22" height="22" />
+                } />
+            </div>
+            <div className={s.Account__info}>
+                <Avatar src={data.src} />
+                <Button
+                    text={data.name}
+                    icon={<Icon name="ArrowDown" width="10" height="5" />}
+                />
+            </div>
+        </div>
+    );
+}
+
+export default Account;
