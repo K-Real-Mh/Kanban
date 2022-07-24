33317589f6d565d9d94034c624a46a251dc04057
Kirill Makhov <mmahovkirill@gmail.com>
2022-07-24 23:52:37 +0300

    refactor: add onClick prop

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,11 +1,19 @@
 import s from './Button.module.scss';
 
-function Button({ icon, text, ariaLabel }) {
+function Button({ icon, text, ariaLabel, onClick }) {
+
+    const handleClick = (event) => {
+        if (onClick) {
+            onClick(event);
+        }
+    }
+
     return (
         <button
             type="button"
             className={s.Button}
             aria-label={ariaLabel}
+            onClick={handleClick}
         >
             {text && <span className={s.Button__text}>{text}</span>}
             {icon}
