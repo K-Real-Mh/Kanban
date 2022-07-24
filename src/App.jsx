0461d338cb8d24d770175a14ee2a39b20e4e8185
Kirill Makhov <mmahovkirill@gmail.com>
2022-07-24 23:55:49 +0300

    feat: add Router component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import s from './App.module.scss';
 import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar.jsx';
+import Router from './components/Router/Router.jsx';
 
 function App() {
   return (
     <div className={s.App}>
         <Header className={s.App__header} />
         <Sidebar className={s.App__sidebar} />
-        <main className={s.App__main}></main>
+        <Router className={s.App__main} />
     </div>
   )
 }
