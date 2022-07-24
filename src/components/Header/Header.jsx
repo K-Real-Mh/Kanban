ee67017e00a2c998d69e511fa039ab6813370751
Kirill Makhov <mmahovkirill@gmail.com>
2022-07-24 23:53:03 +0300

    refactor: move data

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,40 +3,8 @@ import s from './Header.module.scss';
 import Icon from '../Icon/Icon';
 import Navigation from '../Navigation/Navigation';
 import Account from '../Account/Account';
-
-const navigationItems = [
-    {
-        id: 'tasks',
-        label: 'Задачи',
-    },
-    {
-        id: 'projects',
-        label: 'Проекты',
-    },
-    {
-        id: 'statistics',
-        label: 'Статистика',
-    },
-    {
-        id: 'finance',
-        label: 'Финансы',
-    },
-    {
-        id: 'plan',
-        label: 'План',
-    },
-    {
-        id: 'users',
-        label: 'Пользователи',
-    },
-];
-
-const accountData = {
-    sum: 60000,
-    currency: '₽',
-    src: 'https://raw.githubusercontent.com/K-Real-Mh/Kanban/main/static/Nazir.jpg',
-    name: 'Назир',
-}
+import { navigationData } from '../../data/navigationData.js';
+import { accountData } from '../../data/accountData.js';
 
 function Header({ className }) {
     return (
@@ -45,7 +13,7 @@ function Header({ className }) {
                 <Icon width="25" height="24" name="LogoIcon" />
                 <span>eveli.todo</span>
             </a>
-            <Navigation items={navigationItems} active="projects" />
+            <Navigation items={navigationData} active="projects" />
             <Account data={accountData} />
         </header>
     );
